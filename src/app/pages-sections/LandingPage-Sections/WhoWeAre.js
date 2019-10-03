09cd1f9845ae90734d4ba43bd5201e4d378c190f
Erza <erza@emelle.me>
2019-10-03 20:12:40 +0200

    Descriptions about the fields of projects

--- src/app/pages-sections/LandingPage-Sections/WhoWeAre.js
+++ src/app/pages-sections/LandingPage-Sections/WhoWeAre.js
@@ -23,11 +23,9 @@ export default function WhoWeAre() {
         <GridItem xs={12} sm={12} md={8}>
           <h2 className={classes.title}>Who we are</h2>
           <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
+           A team of excited software developers and hardware developers, who like to develop
+           different projects starting from webs, applications, big data and the most special
+           project like electic bikes. 
           </h5>
         </GridItem>
       </GridContainer>
@@ -36,8 +34,9 @@ export default function WhoWeAre() {
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
               title="Software Development"
-              description="
-Entrust full-cycle implementation of your software product to our experienced developers."
+              description="Software development includes a wide field of processing, designing, programming,
+              specifying, documenting and testing. All of these phases are done to create different
+              applications, frameworks and software components"
               icon={Chat}
               iconColor="info"
               vertical
@@ -46,7 +45,10 @@ Entrust full-cycle implementation of your software product to our experienced de
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
               title="Machine Learning"
-              description="We do machine Learning"
+              description="Machine Learning is one of the latest methods of 
+              Artificial Intellegence that involves data analytics to automate
+              analytics of different systems. Its core is to learn from data and then
+               identify patterns so the human interaction would be minimized."
               icon={VerifiedUser}
               iconColor="success"
               vertical
